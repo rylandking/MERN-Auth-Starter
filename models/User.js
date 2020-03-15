e59c2d23ba2f21adac39259c0d664332357e779c
rylandking <ryland@users.noreply.github.com>
2020-03-15 08:02:02 -0600

    Created User model/schema

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('User', UserSchema);
