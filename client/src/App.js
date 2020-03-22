e34d4287d18247f8f52f30153da149dc178d3c16
rylandking <ryland@users.noreply.github.com>
2020-03-21 20:05:19 -0600

    Added PurgeCSS to tailwind config

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p className='bg-blue-500'>
+        <p className='bg-blue-200'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
-  variants: {},
-  plugins: [],
-}
+  variants: { backgroundColor: ['responsive', 'hover', 'focus', 'active'] },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('@tailwindcss/ui')({
+      layout: 'sidebar'
+    }),
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './src/layout/*.js',
+        './public/index.html',
+        './App.js',
+        './index.js'
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  ]
+};
