7c4b4a699fac1b304b697e2e297274a4d1775b8a
rylandking <ryland@users.noreply.github.com>
2020-03-28 16:01:30 -0600

    Login form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,24 +1,47 @@
 import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   const [formData, setFormData] = useState({
+    name: '',
     email: '',
     password: ''
   });
 
-  const { email, password } = formData;
+  const { name, email, password } = formData;
 
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
-    console.log(formData);
+    console.log('Success');
+
+    // const newUser = {
+    //   name,
+    //   email,
+    //   password
+    // };
+
+    // try {
+    //   const config = {
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     }
+    //   };
+
+    //   const body = JSON.stringify(newUser);
+
+    //   const res = await axios.post('/api/users', body, config);
+    //   console.log(res.data);
+    // } catch (error) {
+    //   console.error(error.response.data);
+    // }
   };
 
   return (
     <Fragment>
-      <div className='min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
+      <div className='min-h-screen bg-gray-50 flex flex-col justify-center py-0 sm:px-6 lg:px-8'>
         <div className='sm:mx-auto sm:w-full sm:max-w-md'>
           <img
             className='mx-auto h-12 w-auto'
@@ -28,21 +51,32 @@ const Register = () => {
           <h2 className='mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900'>
             Create your account
           </h2>
-          <p className='mt-2 text-center text-sm leading-5 text-gray-600 max-w'>
-            <span>Or </span>
-            <a
-              href='/'
-              className='font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150'
-            >
-              start your 14-day free trial
-            </a>
-          </p>
         </div>
 
-        <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+        <div className='mt-3 sm:mx-auto sm:w-full sm:max-w-md'>
           <div className='bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10'>
             <form onSubmit={e => onSubmit(e)}>
               <div>
+                <label
+                  htmlFor='email'
+                  className='block text-sm font-medium leading-5 text-gray-700'
+                >
+                  First name
+                </label>
+                <div className='mt-1 rounded-md shadow-sm'>
+                  <input
+                    id='name'
+                    type='name'
+                    name='name'
+                    value={name}
+                    onChange={e => onChange(e)}
+                    required
+                    className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5'
+                  />
+                </div>
+              </div>
+
+              <div className='mt-3'>
                 <label
                   htmlFor='email'
                   className='block text-sm font-medium leading-5 text-gray-700'
@@ -62,7 +96,7 @@ const Register = () => {
                 </div>
               </div>
 
-              <div className='mt-6'>
+              <div className='mt-3'>
                 <label
                   htmlFor='password'
                   className='block text-sm font-medium leading-5 text-gray-700'
@@ -83,40 +117,24 @@ const Register = () => {
                 </div>
               </div>
 
-              <div className='mt-6 flex items-center justify-between'>
-                <div className='flex items-center'>
-                  <input
-                    id='remember_me'
-                    type='checkbox'
-                    className='form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out'
-                  />
-                  <label
-                    htmlFor='remember_me'
-                    className='ml-2 block text-sm leading-5 text-gray-900'
-                  >
-                    Remember me
-                  </label>
-                </div>
-
-                <div className='text-sm leading-5'>
-                  <a
-                    href='/'
-                    className='font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150'
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-              </div>
-
               <div className='mt-6'>
                 <span className='block w-full rounded-md shadow-sm'>
                   <button
                     type='submit'
+                    value='Login'
                     className='w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out'
                   >
-                    Sign in
+                    Create account
                   </button>
                 </span>
+              </div>
+              <div className='text-sm w-full flex justify-center mt-6 leading-5'>
+                <Link
+                  to='/login'
+                  className='font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150'
+                >
+                  Already have an account?
+                </Link>
               </div>
             </form>
           </div>
