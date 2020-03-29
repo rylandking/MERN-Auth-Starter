818abf7efc84c20611f7bc21035c843b6d67bd2c
rylandking <ryland@users.noreply.github.com>
2020-03-29 13:38:25 -0600

    Register user thru redux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,9 +2,10 @@ import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -18,28 +19,9 @@ const Register = ({ setAlert }) => {
 
   const onSubmit = async e => {
     e.preventDefault();
+    register({ name, email, password });
+
     setAlert('Success', 'bg-green-400', 3000);
-
-    // const newUser = {
-    //   name,
-    //   email,
-    //   password
-    // };
-
-    // try {
-    //   const config = {
-    //     headers: {
-    //       'Content-Type': 'application/json'
-    //     }
-    //   };
-
-    //   const body = JSON.stringify(newUser);
-
-    //   const res = await axios.post('/api/users', body, config);
-    //   console.log(res.data);
-    // } catch (error) {
-    //   console.error(error.response.data);
-    // }
   };
 
   return (
@@ -148,7 +130,8 @@ const Register = ({ setAlert }) => {
 };
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
