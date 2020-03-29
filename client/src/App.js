c3c6ddc8a2a5b6d8454a4a88c21a984a4bca9cbe
rylandking <ryland@users.noreply.github.com>
2020-03-29 14:16:39 -0600

    Takes token from localStorage and use it to validate a user on page reloads

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -11,30 +11,42 @@ import Alert from './components/layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <div className='min-h-screen bg-white'>
-          <Navbar />
-          <Route exact path='/' component={Landing} />
-          <section>
-            <Alert />
-            <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/accounts' component={Accounts} />
-              <Route exact path='/transactions' component={Transactions} />
-              <Route exact path='/trends' component={Trends} />
-            </Switch>
-          </section>
-        </div>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <div className='min-h-screen bg-white'>
+            <Navbar />
+            <Route exact path='/' component={Landing} />
+            <section>
+              <Alert />
+              <Switch>
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
+                <Route exact path='/accounts' component={Accounts} />
+                <Route exact path='/transactions' component={Transactions} />
+                <Route exact path='/trends' component={Trends} />
+              </Switch>
+            </section>
+          </div>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
